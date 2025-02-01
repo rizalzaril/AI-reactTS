12b99d985b0155588aadf107a2cc390a2ea78e18
Rizal <zaril.ziral2020@gmail.com>
2025-02-01 07:31:55 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import "./App.css";
 import Home from "./pages/Home";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 function App() {
   return (
     <>
-      <Home />
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <div className="flex float-end p-2">
+            <UserButton />
+          </div>
+        </SignedIn>
+      </header>
+
+      <SignedIn>
+        <Home /> {/* Render the Home component if the user is signed in */}
+      </SignedIn>
     </>
   );
 }
